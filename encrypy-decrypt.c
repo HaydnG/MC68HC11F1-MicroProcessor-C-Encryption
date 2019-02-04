324b3474a04ebcabfcd6c4f988eecb98253e2caa
HaydnG <haydn.gynn@gmail.com>
2019-02-04 16:40:12 +0000

    Cleanup
    Clean code

--- encrypy-decrypt.c
+++ encrypy-decrypt.c
@@ -2,17 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "Keys.h"
+#include "Keys.h" /*A header file containing the definition of the keys*/
 
 void main(){
 
-
 	char table[6][6],string[37], tableEncrypted[6][6];
-	void fillTable(char[37], char[6][6]),OutputArray(char[6][6]),encryptTable(char [6][6], char [6][6], int [6]), convertToString(char [6][6], char [37]);
-	int keyID, *Key,i;
+	void OutputArray(char[6][6]);
+	int keyID, *Key;
 
 
-	clearString(string); /*Pad the string out with spaces, to clear the memory*/
+	clearString(string); /*Pad the string out with spaces, to clear the memory, the string can then be inserted into a clear array*/
 	
 	
 	/*Get interger between 1-720(inclusive) for KeyID*/
@@ -35,14 +34,10 @@ void main(){
 	}
 	
 	/*Pass 1*/
-	fillTable(string, table); /* Fill table with inputted string*/
-	encryptTable(table, tableEncrypted, Key);
-	convertToString(tableEncrypted, string);
+	encryption(string, table, Key);
 
 	/*Pass 2*/
-	fillTable(string, table);
-	encryptTable(table, tableEncrypted, Key);
-	convertToString(tableEncrypted, string);
+	encryption(string, table, Key);
 
 
 	printf("\nEncrypted string: %s\n", string);
@@ -50,36 +45,13 @@ void main(){
 
 }
 
-void convertToString(char table[6][6], char string[37]){
-	int row, letter, count;
-	count = 0;
-	for(row = 0;row< 6; row++){
-		for(letter = 0; letter < 6; letter++){
-					
-			string[count] = table[row][letter];						
-			count++;
-		}		
-	}
-	string[37] = '\0'; /*End string*/
-}
-
-void encryptTable(char table[6][6], char encryptedTable[6][6], int Key[6]){
-	int i,j;
+void encryption(char string[37], char table[6][6], int Key[6]){
+	char tableEncrypted[6][6];
+	void fillTable(char[37], char[6][6]),encryptTable(char [6][6], char [6][6], int [6]), encryptTableToString(char [6][6], char [37]);
 	
-	for(i = 0; i < 6; i++){			
-		for(j = 0; j < 6; j++){
-			encryptedTable[i][j] = table[Key[i]-1][j];	/* move the contents of the table into the encrypted table, in the order of the key*/
-		}		
-	}
-}
-
-void clearString(char string[37]){
-	int i;
-	
-	for(i = 0;i<37;i++){
-		string[i] = ' ';
-	}
-	string[37] = '\0';
+	fillTable(string, table); /* Fill table with inputted string*/
+	encryptTable(table, tableEncrypted, Key); /* Encrypt table with given key and output into specified table*/
+	encryptTableToString(tableEncrypted, string); /* Convert the table back to a string*/
 }
 
 void fillTable(char string[37], char table[6][6]){
@@ -101,6 +73,41 @@ void fillTable(char string[37], char table[6][6]){
 		row++;		
 	}	
 }
+
+void encryptTable(char table[6][6], char encryptedTable[6][6], int Key[6]){
+	int i,j;
+	
+	for(i = 0; i < 6; i++){			
+		for(j = 0; j < 6; j++){
+			encryptedTable[i][j] = table[Key[i]-1][j];	/* move the contents of the table into the encrypted table, in the order of the key*/
+		}		
+	}
+}
+
+void encryptTableToString(char table[6][6], char string[37]){
+	int row, letter, count;
+	count = 0;
+	for(row = 0;row< 6; row++){
+		for(letter = 0; letter < 6; letter++){
+					
+			string[count] = table[row][letter];						
+			count++;
+		}		
+	}
+	string[37] = '\0'; /*End string*/
+}
+
+
+void clearString(char string[37]){
+	int i;
+	
+	for(i = 0;i<37;i++){
+		string[i] = ' ';
+	}
+	string[37] = '\0';
+}
+
+
 
 void OutputArray(char table[6][6]){
 	int i, j;
